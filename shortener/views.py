94428079fac1d7f181ac576005a1c4c9c719d67d
VictorKanama <victor_kanama@hotmail.com>
2021-02-06 19:19:16 +0200

    views modified

--- shortener/views.py
+++ shortener/views.py
@@ -19,5 +19,5 @@ def create(request):
 
 def go(request, pk):
   url_details = Url.objects.get(uuid=pk)
-  return redirect('https:' + url_details.link)
+  return redirect('https://'+ url_details.link)
 
